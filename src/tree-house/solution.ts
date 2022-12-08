12c638a77ebe14f100ae83a193496b6fd9860fa1
rommguy <rommguy@gmail.com>
2022-12-08 23:33:54 +0200

    tree house full solution

--- src/tree-house/solution.ts
+++ src/tree-house/solution.ts
@@ -1,5 +1,6 @@
+import { maxBy, max, takeRight, flatten } from "lodash/fp";
 export const getVisibleIndexes = (line: string[]): number[] => {
-  let max = "0";
+  let max = "";
   const visibleIndexes = new Set<number>();
 
   for (let i = 0; i < line.length; i++) {
@@ -10,7 +11,7 @@ export const getVisibleIndexes = (line: string[]): number[] => {
     }
   }
 
-  max = "0";
+  max = "";
 
   for (let j = line.length - 1; j >= 0; j--) {
     const current = line[j] as string;
@@ -21,4 +22,99 @@ export const getVisibleIndexes = (line: string[]): number[] => {
   }
 
   return [...visibleIndexes.values()];
+};
+
+export const getColumns = (input: string): string[][] => {
+  const lines = input.split("\n").map((line) => [...line]);
+  const columns = lines[0]!.map((_, columnIndex) =>
+    lines.map((line) => line[columnIndex] as string)
+  );
+
+  return columns;
+};
+
+export const getRows = (input: string): string[][] =>
+  input.split("\n").map((line) => [...line]);
+
+export const solution = (input: string): number => {
+  const rows = getRows(input);
+  const columns = getColumns(input);
+  const visibleLocations = new Set();
+
+  rows.map((row, rowIndex) => {
+    getVisibleIndexes(row).forEach((columnIndex) => {
+      visibleLocations.add(`${rowIndex}-${columnIndex}`);
+    });
+  });
+
+  columns.map((column, columnIndex) => {
+    getVisibleIndexes(column).forEach((rowIndex) => {
+      visibleLocations.add(`${rowIndex}-${columnIndex}`);
+    });
+  });
+
+  return visibleLocations.size;
+};
+
+interface TreeScore {
+  start: number;
+  end: number;
+}
+
+const calcScenicScoreFromStart = (line: string[]): number[] => {
+  const result = line.map((tree) => -1);
+  const indexes = new Array(10).fill(-1);
+  for (let i = 0; i < line.length; i++) {
+    const treeHeight = parseInt(line[i] as string, 10);
+    const closestSameOrHigherTree = max(
+      takeRight(indexes.length - treeHeight, indexes)
+    );
+
+    const score =
+      closestSameOrHigherTree > -1 ? i - closestSameOrHigherTree : i;
+
+    result[i] = score;
+
+    indexes[treeHeight] = i;
+  }
+
+  return result;
+};
+export const calcScenicScore = (line: string[]): TreeScore[] => {
+  const startScores = calcScenicScoreFromStart(line);
+  const endScores = calcScenicScoreFromStart(line.reverse()).reverse();
+
+  return startScores.map((startScore, index) => ({
+    start: startScore,
+    end: endScores[index] as number,
+  }));
+};
+
+export const findMaxScenicScore = (input: string): number => {
+  const rows = getRows(input);
+  const columns = getColumns(input);
+
+  const rowsResults = rows.map(calcScenicScore);
+  const columnResults = columns.map(calcScenicScore);
+
+  const allResultsMatrix = columnResults.map((columnResults, columnIndex) =>
+    columnResults.map((columnTree, rowIndex) => {
+      const treeRowResult = rowsResults[rowIndex]![columnIndex] as TreeScore;
+      const treeColumnResult = columnTree as TreeScore;
+      return {
+        rowStart: treeRowResult.start,
+        rowEnd: treeRowResult.end,
+        columnStart: treeColumnResult.start,
+        columnEnd: treeColumnResult.end,
+        score:
+          treeRowResult.start *
+          treeRowResult.end *
+          treeColumnResult.start *
+          treeColumnResult.end,
+      };
+    })
+  );
+  const allResults = flatten(allResultsMatrix) as Array<{ score: number }>;
+
+  return maxBy("score", allResults)!.score;
 };
--- src/tree-house/treeHouse.spec.ts
+++ src/tree-house/treeHouse.spec.ts
@@ -1,13 +1,78 @@
-import { sample } from "./input";
-import { getVisibleIndexes } from "./solution";
+import { sample, input } from "./input";
+import {
+  calcScenicScore,
+  findMaxScenicScore,
+  getColumns,
+  getRows,
+  getVisibleIndexes,
+  solution,
+} from "./solution";
 
 describe("Tree house problem", () => {
-  it("should find all trees that are visible", () => {
+  it("should find all trees that are visible in a line", () => {
     const sample1 = "543234565";
     const sample2 = "123454321";
 
     const res = getVisibleIndexes(sample2.split(""));
     expect(getVisibleIndexes(sample1.split(""))).toEqual([0, 7, 8]);
     expect(res.sort()).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+
+  it("should get columns", () => {
+    const res = getColumns(sample);
+
+    expect(res.length).toEqual(5);
+    expect(res[1]).toEqual(["0", "5", "5", "3", "5"]);
+  });
+
+  it("should get rows", () => {
+    const res = getRows(sample);
+
+    expect(res.length).toEqual(5);
+    expect(res[2]).toEqual(["6", "5", "3", "3", "2"]);
+  });
+
+  describe("solution", () => {
+    it("should count visible trees in sample", () => {
+      const result = solution(sample);
+
+      expect(result).toEqual(21);
+    });
+
+    it("should count visible trees in input", () => {
+      const result = solution(input);
+
+      expect(result).toEqual(1717);
+    });
+
+    describe("scenic score", () => {
+      it("should calc scores for each tree", () => {
+        const sample1 = ["2", "3", "4", "8", "3", "2", "1", "6"];
+        const result = calcScenicScore(sample1);
+
+        expect(result.length).toEqual(sample1.length);
+        expect(result[0]!.start).toEqual(0);
+        expect(result[1]!.start).toEqual(1);
+        expect(result[2]!.start).toEqual(2);
+        expect(result[3]!.start).toEqual(3);
+        expect(result[7]!.start).toEqual(4);
+
+        expect(result[0]!.end).toEqual(1);
+        expect(result[3]!.end).toEqual(4);
+        expect(result[5]!.end).toEqual(2);
+      });
+
+      it("should find the tree with the highest scenic score in sample", () => {
+        const result = findMaxScenicScore(sample);
+
+        expect(result).toEqual(8);
+      });
+
+      it("should find the tree with the highest scenic score in input", () => {
+        const result = findMaxScenicScore(input);
+
+        expect(result).toEqual(321975);
+      });
+    });
   });
 });
