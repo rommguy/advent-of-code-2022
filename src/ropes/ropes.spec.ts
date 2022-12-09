08745d1a79a793cee47cfa2163aee45e10915dd5
rommguy <rommguy@gmail.com>
2022-12-09 14:43:42 +0200

    full solution for n knots

--- src/ropes/ropes.spec.ts
+++ src/ropes/ropes.spec.ts
@@ -4,43 +4,85 @@ import { input, sample } from "./input";
 describe("ropes bridge", () => {
   describe("calculating next position", () => {
     it("should move tail if needed", () => {
-      const newPos = getNextPositions([1, 1], [1, 2], "R", 1);
+      const newPos = getNextPositions(
+        [
+          [1, 2],
+          [1, 1],
+        ],
+        "R",
+        1
+      );
 
-      expect(newPos.head).toEqual([1, 3]);
-      expect(newPos.tail).toEqual([1, 2]);
+      expect(newPos[0]).toEqual([1, 3]);
+      expect(newPos[1]).toEqual([1, 2]);
     });
 
     it("should not move tail if head and tail are still connected", () => {
-      const newPos = getNextPositions([1, 1], [2, 2], "L", 1);
+      const newPos = getNextPositions(
+        [
+          [2, 2],
+          [1, 1],
+        ],
+        "L",
+        1
+      );
 
-      expect(newPos.tail).toEqual([1, 1]);
+      expect(newPos[1]).toEqual([1, 1]);
     });
 
     it("should allow head to go over tail", () => {
-      const newPos = getNextPositions([1, 1], [1, 2], "L", 1);
+      const newPos = getNextPositions(
+        [
+          [1, 2],
+          [1, 1],
+        ],
+        "L",
+        1
+      );
 
-      expect(newPos.tail).toEqual([1, 1]);
+      expect(newPos[1]).toEqual([1, 1]);
     });
 
     it("should combine diagonal with straight movements", () => {
-      const newPos = getNextPositions([10, 1], [10, 2], "D", 4);
+      const newPos = getNextPositions(
+        [
+          [9, 2],
+          [10, 1],
+        ],
+        "D",
+        1
+      );
 
-      expect(newPos.head).toEqual([6, 2]);
-      expect(newPos.tail).toEqual([7, 2]);
+      expect(newPos[0]).toEqual([8, 2]);
+      expect(newPos[1]).toEqual([9, 2]);
     });
   });
 
   describe("counting distinct positions", () => {
     it("should count distinct locations where the tail visits - sample", () => {
-      const result = countDistinctTailPositions(sample);
+      const result = countDistinctTailPositions(sample, 2);
 
       expect(result).toEqual(13);
     });
 
     it("should count distinct locations where the tail visits - input", () => {
-      const result = countDistinctTailPositions(input);
+      const result = countDistinctTailPositions(input, 2);
 
       expect(result).toEqual(6311);
+    });
+  });
+
+  describe("more knots!", () => {
+    it("should count distinct locations where the tail visits - sample", () => {
+      const result = countDistinctTailPositions(sample, 10);
+
+      expect(result).toEqual(1);
+    });
+
+    it("should count distinct locations where the tail visits - input", () => {
+      const result = countDistinctTailPositions(input, 10);
+
+      expect(result).toEqual(2482);
     });
   });
 });
--- src/ropes/solution.ts
+++ src/ropes/solution.ts
@@ -1,6 +1,8 @@
+import { takeRight, last } from "lodash/fp";
+
 type Pos = [number, number];
 
-type Direction = "R" | "U" | "L" | "D";
+type Direction = "R" | "U" | "L" | "D" | "UR" | "UL" | "DR" | "DL";
 
 const getNextHeadPosition = (
   headPos: Pos,
@@ -16,39 +18,105 @@ const getNextHeadPosition = (
       return [headPos[0], headPos[1] + amount];
     case "L":
       return [headPos[0], headPos[1] - amount];
+    case "UR":
+      return [headPos[0] + amount, headPos[1] + amount];
+    case "UL":
+      return [headPos[0] + amount, headPos[1] - amount];
+    case "DR":
+      return [headPos[0] - amount, headPos[1] + amount];
+    case "DL":
+      return [headPos[0] - amount, headPos[1] - amount];
   }
 };
-export const getNextPositions = (
-  tailPos: Pos,
-  headPos: Pos,
-  direction: Direction,
-  amount: number
-): { tail: Pos; head: Pos } => {
-  const newHeadPos = getNextHeadPosition(headPos, direction, amount);
+
+export const calcNextTailPos = (newHeadPos: Pos, tailPos: Pos): Pos => {
   const horizontalDistance = Math.abs(newHeadPos[1] - tailPos[1]);
   const verticalDistance = Math.abs(newHeadPos[0] - tailPos[0]);
   if (horizontalDistance <= 1 && verticalDistance <= 1) {
-    return { tail: tailPos, head: newHeadPos };
+    return tailPos;
   }
 
   if (horizontalDistance > verticalDistance) {
     const directionSign = newHeadPos[1] > tailPos[1] ? 1 : -1;
-    return {
-      tail: [newHeadPos[0], newHeadPos[1] - directionSign],
-      head: newHeadPos,
-    };
+    return [newHeadPos[0], newHeadPos[1] - directionSign];
   }
 
   const directionSign = newHeadPos[0] > tailPos[0] ? 1 : -1;
-  return {
-    tail: [newHeadPos[0] - directionSign, newHeadPos[1]],
-    head: newHeadPos,
-  };
+  return [newHeadPos[0] - directionSign, newHeadPos[1]];
 };
 
 type Action = [Direction, number];
-export const countDistinctTailPositions = (input: string): number => {
-  const lines = input.split("\n");
+
+const getVerticalDiffAction = (verticalDiff: number): Action => {
+  if (verticalDiff === 0) {
+    throw new Error("no vertical diff");
+  }
+  if (verticalDiff > 0) {
+    return ["D", verticalDiff];
+  }
+
+  return ["U", verticalDiff * -1];
+};
+
+const getHorizontalDiffAction = (horizontalDiff: number): Action => {
+  if (horizontalDiff === 0) {
+    throw new Error("no horizontal diff");
+  }
+
+  if (horizontalDiff > 0) {
+    return ["L", horizontalDiff];
+  }
+
+  return ["R", horizontalDiff * -1];
+};
+const calcAction = (initialPos: Pos, newPos: Pos): Action => {
+  const verticalDiff = initialPos[0] - newPos[0];
+  const horizontalDiff = initialPos[1] - newPos[1];
+  if (horizontalDiff === 0 && verticalDiff === 0) {
+    throw new Error("no diff");
+  }
+
+  if (verticalDiff !== 0 && horizontalDiff !== 0) {
+    const verticalAction = getVerticalDiffAction(verticalDiff);
+    const horizontalAction = getHorizontalDiffAction(horizontalDiff);
+    return [`${verticalAction[0]}${horizontalAction[0]}` as Direction, 1];
+  } else if (verticalDiff !== 0) {
+    return getVerticalDiffAction(verticalDiff);
+  }
+  return getHorizontalDiffAction(horizontalDiff);
+};
+
+export const getNextPositions = (
+  positions: Pos[],
+  direction: Direction,
+  amount: number
+): Pos[] => {
+  const headPos = positions[0] as Pos;
+  const tailPos = positions[1] as Pos;
+  const positionsWithoutHead = takeRight(positions.length - 1, positions);
+  const newHeadPos = getNextHeadPosition(headPos, direction, amount);
+  if (positions.length < 2) {
+    return [newHeadPos];
+  }
+
+  const newTailPos = calcNextTailPos(newHeadPos, tailPos);
+
+  if (newTailPos[0] === tailPos[0] && newTailPos[1] === tailPos[1]) {
+    return [newHeadPos, ...positionsWithoutHead];
+  }
+
+  const newAction: Action = calcAction(tailPos, newTailPos);
+  // diagonal will result in multiple actions
+  const restPositions = getNextPositions(
+    positionsWithoutHead,
+    newAction[0],
+    newAction[1]
+  );
+
+  return [newHeadPos, ...restPositions];
+};
+
+const parseLinesToSingleStepActions = (lines: string[]): Action[] => {
   const initialActions: Action[] = lines.map((line) => {
     const [direction, amount] = line.split(" ");
     return [direction, parseInt(amount!, 10)] as [Direction, number];
@@ -64,31 +132,36 @@ export const countDistinctTailPositions = (input: string): number => {
     })
     .flat();
 
-  const initialHeadPosition: Pos = [100, 100];
-  const initialTailPosition: Pos = [100, 100];
+  return actions;
+};
+export const countDistinctTailPositions = (
+  input: string,
+  knotsCount: number
+): number => {
+  const lines = input.split("\n");
+  const actions = parseLinesToSingleStepActions(lines);
+
+  const initialPositions = new Array(knotsCount).fill([100, 100]);
 
   const result = actions.reduce(
     (accm, action) => {
-      const actionResult = getNextPositions(
-        accm.tailPos,
-        accm.headPos,
+      const updatedPositions = getNextPositions(
+        accm.positions,
+
         action[0],
         action[1]
       );
-      accm.distinctPositions.add(
-        `${actionResult.tail[0]}-${actionResult.tail[1]}`
-      );
+      const tailPos = last(updatedPositions) as Pos;
+      accm.distinctPositions.add(`${tailPos[0]}-${tailPos[1]}`);
 
       return {
         ...accm,
-        tailPos: actionResult.tail,
-        headPos: actionResult.head,
+        positions: updatedPositions,
       };
     },
     {
       distinctPositions: new Set(),
-      tailPos: initialTailPosition,
-      headPos: initialHeadPosition,
+      positions: initialPositions,
     }
   );
 
